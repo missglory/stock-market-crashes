fd12f32fe193c724acfdd87c738ce198e9b9bfc7
Dmitriy Lyuosev <xhaustful@gmail.com>
2023-02-26 19:56:21 +0300

    test

---
+++ test-server.py
@@ -0,0 +1,64 @@
+
+
+
+import numpy as np
+import pandas as pd
+
+#Data Source
+import yfinance as yf
+
+#Data viz
+import plotly.graph_objs as go
+
+# Get Bitcoin data
+data = yf.download(tickers='BTC-USD', period = '22h', interval = '15m')
+
+#declare figure
+fig = go.Figure()
+
+#Candlestick
+fig.add_trace(go.Candlestick(x=data.index,
+                open=data['Open'],
+                high=data['High'],
+                low=data['Low'],
+                close=data['Close'], name = 'market data'))
+
+# Add titles
+fig.update_layout(
+    title='Bitcoin live share price evolution',
+    yaxis_title='Bitcoin Price (kUS Dollars)')
+
+# X-Axes
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=15, label="15m", step="minute", stepmode="backward"),
+            dict(count=45, label="45m", step="minute", stepmode="backward"),
+            dict(count=1, label="HTD", step="hour", stepmode="todate"),
+            dict(count=6, label="6h", step="hour", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+#Show
+fig.show()
+
+
+
+# import plotly.graph_objects as go # or plotly.express as px
+# fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
+# fig.add_trace( ... )
+# fig.update_layout( ... )
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
